f131e85f8c6818718c41fa22262c97e0257e2cc5
coderILKIN <99831207+coderILKIN@users.noreply.github.com>
2022-05-11 13:11:11 +0400

    Add files via upload

---
+++ drag and drop/assets/js/script.js
@@ -0,0 +1,11 @@
+let items = document.querySelectorAll('.drop-item');
+
+document.item.style.backgroundColor = "red";
+
+items.forEach(item =>{
+    item.addEventListener('drag',()=>{
+        console.log("dragging")
+        
+  
+    })
+})
